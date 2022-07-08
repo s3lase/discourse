85a83024442337748be7c154b640311a3d5c3b55
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-07-08 20:21:09 +0400

    CLIENT: show status expiration date and time - in progress

--- app/assets/javascripts/discourse/tests/integration/components/user-status-message-tooltip-test.js
+++ app/assets/javascripts/discourse/tests/integration/components/user-status-message-tooltip-test.js
@@ -1,31 +1,119 @@
-import { setupRenderingTest } from "ember-qunit";
+import componentTest, {
+  setupRenderingTest,
+} from "discourse/tests/helpers/component-test";
 import { hbs } from "ember-cli-htmlbars";
-import { discourseModule, exists, query } from "../../helpers/qunit-helpers";
-import componentTest from "../../helpers/component-test";
+import {
+  discourseModule,
+  exists,
+  fakeTime,
+  query,
+} from "discourse/tests/helpers/qunit-helpers";
 
 discourseModule(
   "Integration | Component | user-status-message-tooltip",
   function (hooks) {
     setupRenderingTest(hooks);
 
-    componentTest("it renders", {
+    hooks.afterEach(function () {
+      if (this.clock) {
+        this.clock.restore();
+      }
+    });
+
+    componentTest("it renders status emoji and description", {
       template: hbs`<UserStatusMessageTooltip @status={{this.status}} />`,
 
       beforeEach() {
-        this.set("status", { emoji: "tooth", description: "off to dentist" });
+        this.set("status", {
+          emoji: "tooth",
+          description: "off to dentist",
+        });
       },
 
       async test(assert) {
+        assert.ok(exists("img.emoji[title='tooth']"), "shows the status emoji");
         assert.ok(
-          exists("img.emoji[title='tooth']"),
-          "the status emoji is shown"
-        );
-        assert.equal(
-          query("div.user-status-message-tooltip").textContent.trim(),
-          "off to dentist",
-          "the status description is shown"
+          query("div.user-status-message-tooltip")
+            .textContent.trim()
+            .includes("off to dentist"),
+          "show the status description"
         );
       },
     });
+
+    componentTest("it shows until time if status will expire today", {
+      template: hbs`<UserStatusMessageTooltip @status={{this.status}} />`,
+
+      beforeEach() {
+        this.clock = fakeTime(
+          "2100-02-01T08:00:00.000Z",
+          this.currentUser.timezone,
+          true
+        );
+        this.set("status", {
+          emoji: "tooth",
+          description: "off to dentist",
+          ends_at: "2100-02-01T12:30:00.000Z",
+        });
+      },
+
+      async test(assert) {
+        assert.equal(
+          query("div.user-status-message-tooltip span").textContent.trim(),
+          "Until: 12:30pm"
+        );
+      },
+    });
+
+    componentTest("it shows 'Until tomorrow' if status will expire tomorrow", {
+      template: hbs`<UserStatusMessageTooltip @status={{this.status}} />`,
+
+      beforeEach() {
+        this.clock = fakeTime(
+          "2100-02-01T08:00:00.000Z",
+          this.currentUser.timezone,
+          true
+        );
+        this.set("status", {
+          emoji: "tooth",
+          description: "off to dentist",
+          ends_at: "2100-02-02T12:30:00.000Z",
+        });
+      },
+
+      async test(assert) {
+        assert.equal(
+          query("div.user-status-message-tooltip span").textContent.trim(),
+          "Until tomorrow"
+        );
+      },
+    });
+
+    componentTest(
+      "it shows until date if status will expire the day after tomorrow",
+      {
+        template: hbs`<UserStatusMessageTooltip @status={{this.status}} />`,
+
+        beforeEach() {
+          this.clock = fakeTime(
+            "2100-02-01T08:00:00.000Z",
+            this.currentUser.timezone,
+            true
+          );
+          this.set("status", {
+            emoji: "tooth",
+            description: "off to dentist",
+            ends_at: "2100-02-03T12:30:00.000Z",
+          });
+        },
+
+        async test(assert) {
+          assert.equal(
+            query("div.user-status-message-tooltip span").textContent.trim(),
+            "Until: Feb 3"
+          );
+        },
+      }
+    );
   }
 );
