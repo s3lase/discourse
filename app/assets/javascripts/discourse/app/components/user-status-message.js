0f909e53107e83c4422b23a763712a95419e1d88
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-07-09 22:24:30 +0400

    CLIENT: showing and hiding tooltip - in progress

--- app/assets/javascripts/discourse/app/components/user-status-message.js
+++ app/assets/javascripts/discourse/app/components/user-status-message.js
@@ -1,15 +1,40 @@
 import Component from "@ember/component";
 import { action } from "@ember/object";
 import { createPopper } from "@popperjs/core";
+import { schedule } from "@ember/runloop";
+import { tracked } from "@glimmer/tracking";
 
-export default Component.extend({
+export default class extends Component {
+  tagName = "";
+  @tracked tooltipIsShown = false;
+
+  didInsertElement() {
+    this._super(...arguments);
+    this._initPopper();
+  }
+
+  willDestroyElement() {
+    this._popper.destroy();
+  }
+
   @action
-  onHover() {
-    const popperAnchor = document.querySelector(".user-status-message");
-    const tooltip = document.querySelector(".user-status-message-tooltip");
-
-    this._popper = createPopper(popperAnchor, tooltip, {
-      placement: "bottom",
+  showTooltip() {
+    this.set("tooltipIsShown", true);
+    schedule("afterRender", () => {
+      this._popper.update();
     });
-  },
-});
+  }
+
+  @action
+  hideTooltip() {
+    this.set("tooltipIsShown", false);
+  }
+
+  _initPopper() {
+    schedule("afterRender", () => {
+      const popperAnchor = document.querySelector(".user-status-message");
+      const tooltip = document.querySelector(".user-status-message-tooltip");
+      this._popper = createPopper(popperAnchor, tooltip);
+    });
+  }
+}
