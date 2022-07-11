941c8dea8b47bdec0ed49b303586f8f37a214310
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-07-11 23:04:33 +0400

    CLIENT: pass options to emoji helper

--- app/assets/javascripts/discourse/app/helpers/emoji.js
+++ app/assets/javascripts/discourse/app/helpers/emoji.js
@@ -3,9 +3,10 @@ import { escapeExpression } from "discourse/lib/utilities";
 import { htmlSafe } from "@ember/template";
 import { helper } from "@ember/component/helper";
 
-function emoji(code) {
+function emoji(code, options) {
   const escaped = escapeExpression(`:${code}:`);
-  return htmlSafe(emojiUnescape(escaped));
+  const e = emojiUnescape(escaped, options);
+  return htmlSafe(e);
 }
 
 export default helper(emoji);
