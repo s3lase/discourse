1c627b2074e1b48c09f7ea6384dd09e282641166
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-07-11 20:56:30 +0400

    CLIENT: move code for showing and hidding tooltip into the d-tooltip component - in progress

--- app/assets/javascripts/discourse/app/components/d-tooltip.js
+++ app/assets/javascripts/discourse/app/components/d-tooltip.js
@@ -1,5 +1,47 @@
 import Component from "@ember/component";
+import { createPopper } from "@popperjs/core";
+import { tracked } from "@glimmer/tracking";
+import { schedule } from "@ember/runloop";
+import { action } from "@ember/object";
 
 export default class DiscourseTooltip extends Component {
   tagName = "";
+  @tracked tooltipIsShown = false;
+
+  didInsertElement() {
+    this._super(...arguments);
+    this._initPopper();
+
+    const popperAnchor = document.querySelector(".user-status-message");
+    popperAnchor.addEventListener("mouseenter", this.showTooltip);
+    popperAnchor.addEventListener("mouseleave", this.hideTooltip);
+  }
+
+  willDestroyElement() {
+    const popperAnchor = document.querySelector(".user-status-message");
+    popperAnchor.removeEventListener("mouseenter", this.showTooltip);
+    popperAnchor.removeEventListener("mouseleave", this.hideTooltip);
+    this._popper.destroy();
+  }
+
+  @action
+  showTooltip() {
+    this.set("tooltipIsShown", true);
+    schedule("afterRender", () => {
+      this._popper.update();
+    });
+  }
+
+  @action
+  hideTooltip() {
+    this.set("tooltipIsShown", false);
+  }
+
+  _initPopper() {
+    schedule("afterRender", () => {
+      const popperAnchor = document.querySelector(".user-status-message");
+      const tooltip = document.querySelector(".d-tooltip");
+      this._popper = createPopper(popperAnchor, tooltip);
+    });
+  }
 }
--- app/assets/javascripts/discourse/app/components/user-status-message.js
+++ app/assets/javascripts/discourse/app/components/user-status-message.js
@@ -1,22 +1,9 @@
 import Component from "@ember/component";
-import { action, computed } from "@ember/object";
-import { createPopper } from "@popperjs/core";
-import { schedule } from "@ember/runloop";
-import { tracked } from "@glimmer/tracking";
+import { computed } from "@ember/object";
 import I18n from "I18n";
 
 export default class extends Component {
   tagName = "";
-  @tracked tooltipIsShown = false;
-
-  didInsertElement() {
-    this._super(...arguments);
-    this._initPopper();
-  }
-
-  willDestroyElement() {
-    this._popper.destroy();
-  }
 
   @computed("status.ends_at")
   get until() {
@@ -35,26 +22,5 @@ export default class extends Component {
     } else {
       return `${until} ${endsAt.format("MMM D")}`;
     }
-  }
-
-  @action
-  showTooltip() {
-    this.set("tooltipIsShown", true);
-    schedule("afterRender", () => {
-      this._popper.update();
-    });
-  }
-
-  @action
-  hideTooltip() {
-    this.set("tooltipIsShown", false);
-  }
-
-  _initPopper() {
-    schedule("afterRender", () => {
-      const popperAnchor = document.querySelector(".user-status-message");
-      const tooltip = document.querySelector(".user-status-message-tooltip");
-      this._popper = createPopper(popperAnchor, tooltip);
-    });
   }
 }
